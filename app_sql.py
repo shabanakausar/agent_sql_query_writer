353dcc18cafd9850fd4a2cb203d685159bec19cb
Shabana Kausar <88205538+shabanakausar@users.noreply.github.com>
2025-04-16 15:02:36 -0700

    Update app_sql.py

--- app_sql.py
+++ app_sql.py
@@ -1,10 +1,6 @@
 import streamlit as st
 from pathlib import Path 
-from langchain.agents import create_sql_agent
-from langchain.sql_database import SQLDatabase
 from langchain.agents.agent_types import AgentType
-from langchain.callbacks import StreamlitCallbackHandler
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from sqlalchemy import create_engine
 import sqlite3
 from langchain_groq import ChatGroq
